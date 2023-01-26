313ae4299be7e304b116ac6107e60f4029aa8dfe
Anastasiya <makeeva.anastasya89@gmail.com>
2023-01-26 23:44:19 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3450;
+        int mile = 20;
+
+        int sumOfMiles = ticketPrice / mile;
+
+        System.out.println("Количество начисленных миль за купленный билет:" + sumOfMiles);
+    }
+}
